6bf196fd7f4895aa34072372b8c4d4287c2e9774
wizardy <wizardhassan99@gmail.com>
2023-10-25 21:22:01 +0100

    commit

--- client/src/components/chatbot/Chatbot.jsx
+++ client/src/components/chatbot/Chatbot.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
-import Message from '../message/Message';
+import './Chatbot.scss';
 
 function Chatbot() {
   const [userMessage, setUserMessage] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
+  const [suggestedPrompts] = useState([
+    'Tell me about your services',
+    'Pricing information',
+    'How to contact support',
+  ]);
 
   const handleUserMessageChange = (event) => {
     setUserMessage(event.target.value);
   };
 
-  const handleSendMessage = () => {
-    const message = { text: userMessage, sender: 'user' };
-    setChatHistory([...chatHistory, message]);
-    // Send the user message to the backend and receive the bot's response
-    // Update the chatHistory state with the bot's response
+  const handleSendMessage = (message) => {
+    // Handle the user message here and send it to the backend
+    // Update the chat history with the bot's response
+    setChatHistory([...chatHistory, { text: message, sender: 'user' }]);
     setUserMessage(''); // Clear the input field
   };
 
@@ -21,7 +25,16 @@ function Chatbot() {
     <div className="chatbot">
       <div className="chat-messages">
         {chatHistory.map((message, index) => (
-          <Message key={index} message={message} />
+          <div key={index} className={`message ${message.sender === 'user' ? 'user-message' : ''}`}>
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <div className="suggested-prompts">
+        {suggestedPrompts.map((prompt, index) => (
+          <button key={index} onClick={() => handleSendMessage(prompt)}>
+            {prompt}
+          </button>
         ))}
       </div>
       <input
@@ -30,7 +43,7 @@ function Chatbot() {
         value={userMessage}
         onChange={handleUserMessageChange}
       />
-      <button onClick={handleSendMessage}>Send</button>
+      <button onClick={() => handleSendMessage(userMessage)}>Send</button>
     </div>
   );
 }
