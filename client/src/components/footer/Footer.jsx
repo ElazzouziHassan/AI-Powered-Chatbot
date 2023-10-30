34374f257dc909b7c9bc210a4705126cdfccb05a
wizardy <wizardhassan99@gmail.com>
2023-10-30 20:43:26 +0100

    footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.scss'; 
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; {new Date().getFullYear()} AI-Powered ChatBot By @wizardy</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
